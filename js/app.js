3221bab3157c523d179e8a137ad2a2ed8d22929e
Caio6599 <caiosantosdamaceno@gmail.com>
2026-05-12 09:59:59 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let login = document.getElementById("login").value;
+let senha = document.getElementById("senha").value;
+
+if (login === jthimpx && senha === Tdf891) {
+
+    alert("bem-vindo")
+
+}else{
+
+    alert("acesso negado")
+} 
+
+document.getElementById("login").value="";
+document.getElementById("senha").value="";
